d0be0eee68dd5dd41f45f1adc8bf482ae268f6e3
Mark Walsh <S00181377@mail.itsligo.ie>
2019-10-02 12:18:29 +0100

    Service Added

---
+++ src/app/service/sygdatabase.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SYGDatabaseService } from './sygdatabase.service';
+
+describe('SYGDatabaseService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: SYGDatabaseService = TestBed.get(SYGDatabaseService);
+    expect(service).toBeTruthy();
+  });
+});
