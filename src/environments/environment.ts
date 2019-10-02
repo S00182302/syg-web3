3ebc9a49960740f82232dd6f9ba672342e6e91d0
Mark Walsh <S00181377@mail.itsligo.ie>
2019-10-02 11:47:07 +0100

    Firebase Added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC0fYu_z7wyWIGxJPQB3ZkLrjIY0DwjQx8",
+    authDomain: "svg-web3.firebaseapp.com",
+    databaseURL: "https://svg-web3.firebaseio.com",
+    projectId: "svg-web3",
+    storageBucket: "svg-web3.appspot.com",
+    messagingSenderId: "181765210669",
+    appId: "1:181765210669:web:d0d723f6f1be3f1899eea4",
+    measurementId: "G-C76EDKL5Q3"
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
