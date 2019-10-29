730796bf5b55f3e5c5d894289bce46d616c07f28
Markw1998 <35930598+Markw1998@users.noreply.github.com>
2019-10-29 00:23:36 +0000

    Blog Page Design/Started

--- src/app/comp/blog/blog.component.ts
+++ src/app/comp/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SYGDatabaseService } from 'src/app/service/sygdatabase.service';
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  public isCollapsed: boolean[] = [];
+  items: any;
 
-  constructor() { }
+  constructor(private fireBaseService: SYGDatabaseService) {}
 
   ngOnInit() {
+    this.fireBaseService.ReadBlogs().subscribe(result => {
+      this.items = result;
+    });
   }
-
 }
