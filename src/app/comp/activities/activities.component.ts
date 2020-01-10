d1f748ca367941c0a2973d8c534a8869646b7196
s00182302@mail.itsligo.ie <s00182302@mail.itsligo.ie>
2020-01-10 22:04:21 +0000

    role check, add activities

--- src/app/comp/activities/activities.component.ts
+++ src/app/comp/activities/activities.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SYGDatabaseService } from 'src/app/service/sygdatabase.service';
 import { Activity } from './activity';
+import { AuthService } from "src/app/service/auth.service";
+import { Router } from "@angular/router";
+import { Observable, of } from 'rxjs';
+import { userModel } from '../../models/userModel';
 
 @Component({
   selector: 'app-activities',
@@ -9,14 +13,45 @@ import { Activity } from './activity';
 })
 export class ActivitiesComponent implements OnInit {
   activities: Activity[];
+  isLoggedIn: boolean = false;
+  isValidRole: boolean;
 
-  constructor(private fireService: SYGDatabaseService) {}
+  constructor(private fireService: SYGDatabaseService, private auth: AuthService, public router: Router) {}
 
   ngOnInit() {
+    this.isLoggedIn = this.auth.isLoggedIn();
+    this.isRequiredRole("admin");
+    //uvalidRole.subscribe(myBool => this.isValidRole = myBool);
+    console.log(this.isValidRole);
+
     this.fireService.GetActivities().subscribe(result => {
       this.activities = result;
     });
+  }
 
+  isRequiredRole(role: string) {
+    let currentUser: userModel;
 
+    this.fireService.getUsers().subscribe(data => 
+      {
+        data.forEach(element => {
+          if(element.UserUID != null && this.auth.user != null){
+            
+            if(element.UserUID === this.auth.user.uid){
+              currentUser = element;console.log("matched");
+
+              if(currentUser != null){
+                currentUser.Role.forEach(r => {
+                  if( r.toLowerCase() === role.toLowerCase()){
+                    this.isValidRole = true;
+                    return false;
+                  }
+                });
+              }
+            }
+          }
+        }); 
+    });
+    return false;
   }
 }
