e4a56a085c4fdf34e66254619013279c9f31c87c
Markw1998 <35930598+Markw1998@users.noreply.github.com>
2020-01-11 01:53:46 +0000

    Error Fixes for Build;

--- src/app/comp/volunteers/volunteer.ts
+++ src/app/comp/volunteers/volunteer.ts
@@ -1,10 +1,10 @@
 export interface Volunteer {
-    id: string;
-    Name?: string;
-    Hobbys?: string;
-    Description?: string;
-    FeatureImage?: string;
-    Age?: number;
-    Email?: string;
-    Mobile?: number;
-  }
+  id: string;
+  Name?: string;
+  Hobbys?: string;
+  Description?: string;
+  FeaturedImage?: string;
+  Age?: number;
+  Email?: string;
+  Mobile?: number;
+}
